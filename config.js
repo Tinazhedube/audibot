6fdf9234aa6c80885b25028742efec186e015e13
Tinashe Dube <tinashedub@gmail.com>
2017-10-08 15:58:32 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: '	Y9U7JPdT4QS6UFQD3Vko9X2OA',  
+  consumer_secret: '	g5f6qowot1u2g4bkPmpqSeRb7oO9Q5mIp2dd9gDDAzaAK34c99',
+  access_token: '	4843475159-9nmqf5XaPUsszfaVgSbagpvrncsEa1cd5NrI7RU',  
+  access_token_secret: '	s4N3NdXbgdX1FVVJJ5EXOMdUdVidf09KQ5WpV0nbNX489'
+}
